861339106f5984a028c2688e5ec8a2be73a6b970
Kabir Deula <kabirdeula167@gmail.com>
2023-03-01 11:58:25 +0545

    added Network Interface

---
+++ Lab/Mar01/src/InterfaceLister.java
@@ -0,0 +1,12 @@
+import java.net.*;
+import java.util.*;
+
+public class InterfaceLister {
+    public static void main(String[] args) throws SocketException {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface ni = interfaces.nextElement();
+            System.out.println(ni);
+        }
+    }
+}
