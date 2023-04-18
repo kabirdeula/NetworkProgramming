10bc232f373ed7df377530b121860a719b9e87d0
kabirdeula <kabirdeula167@gmail.com>
2023-04-18 12:55:26 +0545

    added Client and Server

--- Lab/Apr03/src/Client.java
+++ Lab/Apr03/src/Client.java
@@ -1,50 +1,43 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
-public class Client {
-    private Socket socket = null;
-    private BufferedReader inputStream = null;
-    private DataOutputStream outputStream = null;
-    
-    public Client(String address, int port){
-        try {
-            socket = new Socket(address, port);
-            System.out.println("Connected");
-
-            inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            outputStream = new DataOutputStream(socket.getOutputStream());
-        } catch (UnknownHostException e) {
-            System.out.println(e);
-            return;
-        }catch(IOException e){
-            System.out.println(e);
-            return;
-        }
-        
-        String line = " ";
-        
-        while(!line.equals("Over")){
-            try{
-
-                line = inputStream.readLine();
-                outputStream.writeUTF(line);
-            }catch(IOException e){
-                System.out.println(e);
-            }
-        }
-        
-        try {
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-    }
-
+class Client {
     public static void main(String[] args) {
-        new Client("127.0.0.1", 5000);
-    }
+        try {
+            try (
+                // Reading Host IP address and port given by the user.
+            Scanner scanner = new Scanner(System.in)) {
+                System.out.println("Enter host address: ");
+                String host = scanner.nextLine();
+                System.out.println("Enter port address: ");
+                int port = Integer.parseInt(scanner.nextLine());
 
+                // Connecting to socket using host and port provided by user.
+                Socket socket = new Socket(host, port);
+                System.out.println("Connection to server accepted.");
+
+                // I/o stream to read and write data
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedReader bufferedReader2 = new BufferedReader(new InputStreamReader(System.in));
+
+                String strRequest, strResponse;
+                System.out.println("/'quit/' to exit");
+                while (!(strRequest = bufferedReader2.readLine()).equals("quit")) {
+                    // dataOutputStream to write to the server.
+                    dataOutputStream.writeBytes(strRequest + "\n");
+                    strResponse = bufferedReader.readLine();
+                    System.out.println("Server: " + strResponse);
+                    System.out.println("Client: ");
+                }
+                dataOutputStream.close();
+                bufferedReader.close();
+                bufferedReader2.close();
+                socket.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Lab/Apr03/src/Server.java
+++ Lab/Apr03/src/Server.java
@@ -1,46 +1,55 @@
 import java.io.*;
 import java.net.*;
 
-public class Server {
-    private Socket socket = null;
-    private ServerSocket server = null;
-    private DataInputStream inputStream = null;
-
-    public Server(int port){
-        try {
-            server = new ServerSocket(port);
-            System.out.println("Server started.");
-
-            System.out.println("Waiting for a client ...");
-
-            socket = server.accept();
-            System.out.println("Client accepted.");
-
-            inputStream = new DataInputStream(
-                new BufferedInputStream(socket.getInputStream())
-            );
-
-            String line = " ";
-
-            while (!line.equals("Over")) {
-                try {
-                    line = inputStream.readUTF();
-                    System.out.println(line);
-                } catch (IOException e) {
-                    System.out.println(e);
-                }
-            }
-            
-            System.out.println("Closing connection.");
-            
-            socket.close();
-            inputStream.close();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-    }
-
+class Server{
     public static void main(String[] args) {
-        new Server(5000);
+        try{
+            int port = 7001;
+
+            // Creating a server
+            ServerSocket server = new ServerSocket(port);
+            
+            // getting ip address of the server
+            InetAddress address = InetAddress.getLocalHost();
+
+            System.out.println("Waiting for connection" + "\nHost IP: " + address.getHostAddress() + "\nPort: " + port);
+
+            // Accepting client connection    
+            Socket socket = server.accept();
+
+            System.out.println("Connection type: TCP\n" + "Connection Established");
+
+            // Creating input/output stream for writing data to client or reading the data from client.
+            PrintStream printStream = new PrintStream(socket.getOutputStream());
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            BufferedReader bufferedReader2 = new BufferedReader(new InputStreamReader(System.in));
+
+            while (true) {
+                String strRequest, strResponse = null;
+
+                while ((strRequest = bufferedReader.readLine()) != null) {
+                    System.out.println("Client :: " + strRequest);
+                    if (strRequest.startsWith("/")) {
+                        // custom commands
+                        if (strRequest.startsWith("/hello")) {
+                            System.out.println("Hello World!!");
+                        }
+                    }else{
+                        System.out.println("Server :: ");
+                        strResponse = bufferedReader2.readLine();
+                    }
+                    printStream.println(strResponse);
+                }
+                printStream.close();
+                bufferedReader.close();
+                bufferedReader2.close();
+                server.close();
+                socket.close();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
